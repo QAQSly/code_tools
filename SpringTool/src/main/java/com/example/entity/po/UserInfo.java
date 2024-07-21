7de43ac84ac856682beac3769ade15699c0e7bed
QAQSly <2015672764@qq.com>
2024-07-21 12:31:15 +0800

    update

--- SpringTool/src/main/java/com/example/entity/po/UserInfo.java
+++ SpringTool/src/main/java/com/example/entity/po/UserInfo.java
@@ -13,26 +13,37 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserInfo implements Serializable {
+	//用户ID
 	private String userId;
 
+	//用户昵称
 	private String nickName;
 
+	//邮箱
 	private String email;
 
+	//QQ开放ID
 	private String qqOpenId;
 
+	//QQ头像
 	private String qqAvatar;
 
+	//密码
 	private String password;
 
+	//加入时间
 	private Date joinTime;
 
+	//最后是登录一回
 	private Date lastLoginTime;
 
+	//状态
 	private Integer status;
 
+	//已使用空间
 	private Long useSpace;
 
+	//总空间
 	private Long totalSpace;
 
 }
