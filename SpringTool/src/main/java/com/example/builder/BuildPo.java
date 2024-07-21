9562512198b9f5a4f9a6d6b3724f3947c53e401f
QAQSly <2015672764@qq.com>
2024-07-21 09:24:04 +0800

    update

--- SpringTool/src/main/java/com/example/builder/BuildPo.java
+++ SpringTool/src/main/java/com/example/builder/BuildPo.java
@@ -5,27 +5,32 @@ import com.example.bean.TableInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class BuildPo {
     public static final Logger logger = LoggerFactory.getLogger(BuildPo.class);
     public static void execute(TableInfo tableInfo) {
         File folder = new File(Constants.PATH_PO);
-        logger.info("{}", Constants.PATH_PO);
 
         if (!folder.exists()) {
             folder.mkdirs();
-            logger.info("{}", "创建PO目录");
         }
 
-        try (OutputStream  out = new OutputStream() {
-            @Override
-            public void write(int i) throws IOException {
+        File poFile = new File(folder, tableInfo.getBeanName() + ".java");
 
-            }
-        }) {
+        try (OutputStream  out = new FileOutputStream(poFile);
+             OutputStreamWriter outW = new OutputStreamWriter(out, "utf-8");
+             BufferedWriter bw = new BufferedWriter(outW)) {
+            // package
+            bw.write("package " + Constants.PACKAGE_PO + ";");
+            bw.newLine();
+            bw.write("import java.io.Serializable;");
+            bw.newLine();
+            bw.write("public class " + tableInfo.getBeanName() + " implements Serializable {");
+            bw.newLine();
+            bw.write("}");
+            bw.flush();
+
 
         } catch (IOException e) {
             e.printStackTrace();
