944d7f3b4b889963948d1f15098d7591ffd1436f
QAQSly <2015672764@qq.com>
2024-07-21 10:20:17 +0800

    update

--- SpringTool/src/main/java/com/example/entity/po/UserInfo.java
+++ SpringTool/src/main/java/com/example/entity/po/UserInfo.java
@@ -1,6 +1,31 @@
 package com.example.entity.po;
 
 import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
-public class UserInfo implements Serializable {
+@Getter
+@Setterpublic class UserInfo implements Serializable {
+	private String userId;
+
+	private String nickName;
+
+	private String email;
+
+	private String qqOpenId;
+
+	private String qqAvatar;
+
+	private String password;
+
+	private Date joinTime;
+
+	private Date lastLoginTime;
+
+	private Integer status;
+
+	private Long useSpace;
+
+	private Long totalSpace;
+
 }
