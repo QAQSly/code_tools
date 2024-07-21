64efe4fc31f4987feca475864c642dc0cace401e
QAQSly <2015672764@qq.com>
2024-07-21 09:07:31 +0800

    po

--- SpringTool/src/main/java/com/example/builder/BuildPo.java
+++ SpringTool/src/main/java/com/example/builder/BuildPo.java
@@ -2,17 +2,24 @@ package com.example.builder;
 
 import com.example.bean.Constants;
 import com.example.bean.TableInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class BuildPo {
+    public static final Logger logger = LoggerFactory.getLogger(BuildPo.class);
     public static void execute(TableInfo tableInfo) {
         File folder = new File(Constants.PATH_PO);
 
         if (!folder.exists()) {
             folder.mkdirs();
+            logger.info("{}", "创建PO目录");
         }
+
+
 
     }
 }
