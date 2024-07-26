fc849927b3c59826ae605ac7d1824fc70b4a355c
QAQSly <2015672764@qq.com>
2024-07-26 13:34:42 +0800

    update

--- SpringTool/src/main/java/com/example/entity/po/UserInfo.java
+++ SpringTool/src/main/java/com/example/entity/po/UserInfo.java
@@ -47,7 +47,7 @@ public class UserInfo implements Serializable {
 	private String password;
 
 	//加入时间
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+3")
+	@ToString.Exclude()	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+3")
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date joinTime;
 
@@ -57,7 +57,7 @@ public class UserInfo implements Serializable {
 	}
 
 	//最后是登录一回
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+3")
+	@ToString.Exclude()	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+3")
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date lastLoginTime;
 
@@ -65,7 +65,6 @@ public class UserInfo implements Serializable {
 	public String lastLoginTime() {
 		return DateUtil.format(lastLoginTime, DateTimePatternEnum.YYYY_MM_DD_HH_MM_SS.getPattern());
 	}
-
 
 	//状态
 	@ToString.Include(name = "状态")
@@ -83,10 +82,5 @@ public class UserInfo implements Serializable {
 	//0: deleted, 1: normal
 	@ToString.Include(name = "0: deleted, 1: normal")
 	private Integer isDel;
-	public static void main(String[] args) {
-		UserInfo u = new UserInfo();
-		u.setJoinTime(new Date());
-		u.setLastLoginTime(new Date());
-		System.out.println(u);
-	}
+
 }
